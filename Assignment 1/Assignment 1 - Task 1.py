2da3f98827449853a833d80bad0e8de87fd40585
G Sidhartha <gsidhartha864@gmail.com>
2025-07-12 15:11:46 +0530

    Delete Assignment 1 directory

--- Assignment 1/Assignment 1 - Task 1.py
+++
@@ -1,9 +0,0 @@
-n1= input("enter first number: ")
-n1 = int(n1)
-n2= input("enter second number: ")
-n2 = int(n2)
-
-print("addition :" , n1+n2)
-print("subtraction :" , n1-n2)
-print("multiplication :" , n1*n2)
-print("division :" , n1/n2)
--- Assignment 1/Assignment 1 - Task 2.py
+++
@@ -1,4 +0,0 @@
-n1= input("enter your first name: ")
-n2= input("enter your second name: ")
-
-print("Hello " + n1 + " " + n2 + "! Welcome to the Python program.")
