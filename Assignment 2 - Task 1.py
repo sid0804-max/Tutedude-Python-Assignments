5578e3c30b4486b5ecfe3cc1442e71061dd30be2
G Sidhartha <gsidhartha864@gmail.com>
2025-07-12 20:18:24 +0530

    Add files via upload

---
+++ Assignment 2 - Task 1.py
@@ -0,0 +1,6 @@
+n = int(input("enter your number: "))
+
+if (n % 2 == 0):
+    print(str(n) + " is an even number")
+else:
+    print(str(n) + " is an odd number")
---
+++ Assignment 2 - Task 2.py
@@ -0,0 +1,5 @@
+a = 0
+for i in range(1, 51):  # 52 because range is exclusive of the end
+    a = a + i
+
+print("Sum of numbers from 1 to 50 is:", a)
