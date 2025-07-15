99997bd9251a0e7ab4ce12878a5dceef6c6ab625
G Sidhartha <gsidhartha864@gmail.com>
2025-07-16 02:25:29 +0530

    Delete Assignment 4 directory

--- Assignment 4/Assignment 4 - Task 1.py
+++
@@ -1,8 +0,0 @@
-m1 = "sample1.txt"
-try:
-    print("Reading file content:")
-    with open(m1, "r") as file:
-        read = file.read()
-        print(read)
-except FileNotFoundError:
-    print("Error: The file " + m1 + " was not found.")
